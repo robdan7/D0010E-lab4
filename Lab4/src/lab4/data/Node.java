a00b9d7b58cad51614ddc446101a0006115d2397
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-15 20:15:03 +0100

    Bug fixes and node traversing optimization.

--- Lab4/src/lab4/data/Node.java
+++ Lab4/src/lab4/data/Node.java
@@ -12,18 +12,14 @@ import java.util.HashMap;
  *
  */
 class Node {
-	private HashMapV2i<Node> neighbors;
-	private Vector2i position;
+	// The position lets a node know where on the grid it is. This is easier than to search for the position.
+	private final Vector2i position;
+
 	private OccupiedBy value;
 
 	public Node(OccupiedBy e, int x, int y) {
 		this.value = e;
 		this.position = new Vector2i(x, y);
-		this.neighbors = new HashMapV2i<>();
-	}
-
-	public int getNeighborCount() {
-		return this.getVerticalNeighbors(this) + this.getHorizontalNeighbors(this) + this.getDiagonalNeighbors(this);
 	}
 
 	/**
@@ -31,26 +27,29 @@ class Node {
 	 * @return The longest chain of {@link Node} instances in either a horizontal,
 	 *         vertical or diagonal direction. The longest chain will be returned.
 	 */
-	public int getMaximumChain() {
-		int horizontal = this.getHorizontalNeighbors(this);
-		int vertical = this.getVerticalNeighbors(this);
-		int diagonal = this.getDiagonalNeighbors(this);
-
+	public int getMaximumChain(Node[][] grid) {
+		if (!grid[this.position.x][this.position.y].equals(this)) {
+			throw new IllegalArgumentException("Array does not contain the starting node.");
+		}
+		int horizontal = this.getHorizontalNeighbors(grid, this, 0);
+		int vertical = this.getVerticalNeighbors(grid, this, 0);
+		int[] diagonal = this.getDiagonalNeighbors(grid, this, new int[] { 0, 0 });
+		int sumDiagonal = diagonal[0] > diagonal[1] ? diagonal[0] : diagonal[1];
 		int maximumRow = 0;
 
 		if (horizontal > vertical) {
-			if (horizontal > diagonal) {
+			if (horizontal > sumDiagonal) {
 				maximumRow = horizontal;
 			} else {
 				maximumRow = vertical;
 			}
-		} else if (vertical > diagonal) {
+		} else if (vertical > sumDiagonal) {
 			maximumRow = vertical;
 		} else {
-			maximumRow = diagonal;
+			maximumRow = sumDiagonal;
 		}
 
-		return maximumRow + 1; // +1 -> Include this node.
+		return maximumRow + 1; // +1 -> Include the starting node.
 	}
 
 	public int getX() {
@@ -61,24 +60,43 @@ class Node {
 		return this.position.getY();
 	}
 
+	private Node fetchFromGrid(Node[][] grid, Vector2i v) {
+		try {
+			return grid[v.getX()][v.getY()];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return null;
+		}
+	}
+
 	/**
 	 * Search for horizontal neighbors with the same {@link #getValue()}. The value
 	 * could be stored as a variable, but the solution is not required to be fast.
+	 * <br>
+	 * The number of times to search is decided by {@link GameGrid#INROW} minus 1.
+	 * The starting node does not count as a neighbor because it is already a part
+	 * of the row.
 	 * 
 	 * @param n
-	 *            The previous node.
+	 *            - The previous node.
+	 * @param totalNeighbors
+	 *            - How many horizontal neighbors there are in total.
 	 * @return How many horizontal neighbors there are in a row.
 	 */
-	private int getHorizontalNeighbors(Node n) {
+	private int getHorizontalNeighbors(Node[][] grid, Node n, int totalNeighbors) {
+		if (totalNeighbors == GameGrid.INROW - 1) {
+			return 0;
+		}
 		Vector2i v = this.position.copy(); // Don't mess with the position. Get a copy.
 		int result = 0;
 		for (int start = v.getX(), x = start - 1; x <= start + 1; x += 2) {
 			v.x = x;
-			if (this.neighbors.containsKey(v)) {
-				Node n2 = this.neighbors.get(v);
-				if ((n2.getValue() == this.getValue()) && !n2.equals(n)) {
+			Node neighbor = this.fetchFromGrid(grid, v);
+			if (neighbor != null) {
+				if ((neighbor.getValue() == this.getValue()) && !neighbor.equals(n)) {
+					totalNeighbors++;
 					result++;
-					result += this.neighbors.get(v).getHorizontalNeighbors(this);
+					// Get the neighbors neighbors.
+					result += neighbor.getHorizontalNeighbors(grid, this, totalNeighbors);
 				}
 			}
 		}
@@ -86,24 +104,35 @@ class Node {
 	}
 
 	/**
-	 * Search for horizontal neighbors with the same {@link #getValue()}. The value
+	 * Search for vertical neighbors with the same {@link #getValue()}. The value
 	 * could be stored as a variable, but the solution is not required to be fast.
+	 * <br>
+	 * The number of times to search is decided by {@link GameGrid#INROW} minus 1.
+	 * The starting node does not count as a neighbor because it is already a part
+	 * of the row.
 	 * 
 	 * @param n
-	 *            The previous node.
+	 *            - The previous node.
+	 * @param totalNeighbors
+	 *            - How many vertical neighbors there are in total.
 	 * @return How many vertical neighbors there are in a row.
 	 */
-	private int getVerticalNeighbors(Node n) {
+	private int getVerticalNeighbors(Node[][] grid, Node n, int totalNeighbors) {
+		if (totalNeighbors == GameGrid.INROW - 1) {
+			return 0;
+		}
 		Vector2i v = this.position.copy(); // Don't mess with the position. Get a copy.
 
 		int result = 0;
 		for (int start = v.getY(), y = start - 1; y <= start + 1; y += 2) {
 			v.y = y;
-			if (this.neighbors.containsKey(v)) {
-				Node n2 = this.neighbors.get(v);
-				if ((n2.getValue() == this.getValue()) && !n2.equals(n)) {
+			Node neighbor = this.fetchFromGrid(grid, v);
+			if (neighbor != null) {
+				if ((neighbor.getValue() == this.getValue()) && !neighbor.equals(n)) {
+					totalNeighbors++;
 					result++;
-					result += this.neighbors.get(v).getVerticalNeighbors(this);
+					// Get the neighbors neighbors.
+					result += neighbor.getVerticalNeighbors(grid, this, totalNeighbors);
 				}
 			}
 		}
@@ -111,53 +140,61 @@ class Node {
 	}
 
 	/**
-	 * Search for horizontal neighbors with the same {@link #getValue()}. The value
+	 * Search for diagonal neighbors with the same {@link #getValue()}. The value
 	 * could be stored as a variable, but the solution is not required to be fast.
+	 * <br>
+	 * The number of times to search is decided by {@link GameGrid#INROW} minus 1.
+	 * The starting node does not count as a neighbor because it is already a part
+	 * of the row.
 	 * 
 	 * @param n
-	 *            The previous node.
-	 * @return How many diagonal neighbors there are in a row.
+	 *            - The previous node.
+	 * @param totalNeighbors
+	 *            - Two integers containing the number of neighbors found already.
+	 * @return How many diagonal neighbors there are in total. Since there are two
+	 *         directions a corresponding array is returned.
 	 */
-	private int getDiagonalNeighbors(Node n) {
-		Vector2i v = this.position.copy();
-		int upSlopeResult = 0;
-		int downSlopeResult = 0;
+	private int[] getDiagonalNeighbors(Node[][] grid, Node n, int[] totalNeighbors) {
+		if ((totalNeighbors[0] == GameGrid.INROW - 1) || (totalNeighbors[1] == GameGrid.INROW - 1)) {
+			return new int[] { 0, 0 };
+		} else if (totalNeighbors.length != 2) {
+			throw new IllegalArgumentException("Array does not contain two integers");
+		}
 
-		int i = 0; // this takes care of the direction of the diagonal pattern.
-		for (int startX = v.getX(), x = startX - 1; x <= startX + 1; x += 2) {
+		Vector2i v = this.position.copy(); // Don't mess with the position. Get a copy.
+
+		int[] result = new int[2];
+		int loopPattern = 0; // this takes care of the direction of the diagonal pattern.
+
+		int startX = v.getX();
+		int startY = v.getY();
+		for (int x = startX - 1; x <= startX + 1; x += 2) {
 			v.x = x;
-			for (int startY = v.getY(), y = startY - 1; y <= startY + 1; y += 2) {
+			for (int y = startY - 1; y <= startY + 1; y += 2) {
 				v.y = y;
-				if (this.neighbors.containsKey(v)) {
-					Node n2 = this.neighbors.get(v);
-					int result = 0;
-					if ((n2.getValue() == this.getValue()) && !n2.equals(n)) {
-						result++;
-						result += this.neighbors.get(v).getDiagonalNeighbors(this);
-					}
-					if (i == 0 || i == 3) {
-						downSlopeResult += result;
-					} else {
-						upSlopeResult += result;
+				Node neighbor = this.fetchFromGrid(grid, v);
+				if (neighbor != null) {
+					if ((neighbor.getValue() == this.getValue()) && !neighbor.equals(n)) {
+						if ((loopPattern == 0) || (loopPattern == 3)) {
+							totalNeighbors[0]++;
+							result[0] ++;
+						} else {
+							totalNeighbors[1]++;
+							result[1] ++;
+						}
+						// Get the neighbors neighbors.
+						int[] temp = neighbor.getDiagonalNeighbors(grid, this, totalNeighbors);
+						
+						// Add the recursive result to the next one.
+						result[0] += temp[0];
+						result[1] += temp[1];
 					}
 				}
-
-				i++;
+				loopPattern++;
 			}
-			v.y = this.position.getY(); // v.y has been altered. Reset it.
+			v.y = startY; // v.y has been altered. Reset it.
 		}
-		return upSlopeResult > downSlopeResult ? upSlopeResult : downSlopeResult;
-	}
-
-	public boolean addNeighbor(Node n, int x, int y) {
-		Vector2i v = new Vector2i(x, y);
-		if (!this.neighbors.containsKey(v)) {
-			this.neighbors.put(v, n);
-			n.addNeighbor(this, this.position.getX(), this.position.getY());
-			return true;
-		}
-
-		return false;
+		return result;
 	}
 
 	/**
@@ -167,64 +204,39 @@ class Node {
 	public OccupiedBy getValue() {
 		return this.value;
 	}
-	
+
+	public void setValue(OccupiedBy o) {
+		this.value = o;
+	}
+
 	public static enum OccupiedBy {
 		EMPTY, ME, OTHER
 	}
 
 	/**
-	 * HashMap class with extra features. The keys are instances of {@link Vector2i}
+	 * Vector with two integers.
 	 * 
 	 * @author Robin Danielsson
 	 *
-	 * @param <V>
 	 */
-	private class HashMapV2i<V> extends HashMap<Vector2i, V> {
-		private static final long serialVersionUID = 1L;
+	private class Vector2i {
+		private int x, y;
 
-		private ArrayList<Vector2i> keys;
-
-		public HashMapV2i() {
-			super();
-			this.keys = new ArrayList<>();
+		public int getX() {
+			return this.x;
 		}
 
-		@Override
-		public V put(Vector2i v, V obj) {
-			if (!this.containsKey(v)) {
-				this.keys.add(v);
-				super.put(v, obj);
-			}
-			return obj;
+		public int getY() {
+			return this.y;
 		}
 
-		@Override
-		public boolean containsKey(Object key) {
-			boolean result = false;
-			if (key instanceof Vector2i) {
-				Vector2i keycopy = this.getKey((Vector2i) key);
-				if (!super.containsKey(keycopy)) {
-					this.keys.remove(keycopy);
-				} else {
-					result = true;
-				}
-			}
-			return result;
+		public Vector2i(int x, int y) {
+			this.x = x;
+			this.y = y;
 		}
 
-		@Override
-		public V get(Object key) {
-			return super.get(this.getKey((Vector2i) key));
-		}
-
-		public Vector2i getKey(Vector2i key) {
-			Vector2i k = (Vector2i) key;
-			for (Vector2i v : this.keys) {
-				if (v.getX() == k.getX() && v.getY() == k.getY()) {
-					return v;
-				}
-			}
-			return null;
+		public Vector2i copy() {
+			return new Vector2i(this.getX(), this.getY());
 		}
 	}
 }
