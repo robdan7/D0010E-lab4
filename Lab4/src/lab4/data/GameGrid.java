5d2c5942f77dc90d412fbc732b5f4d07a4ca3e1f
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-15 20:15:49 +0100

    Grid remake and node implementation done :)

--- Lab4/src/lab4/data/GameGrid.java
+++ Lab4/src/lab4/data/GameGrid.java
@@ -8,97 +8,140 @@ import java.util.Observable;
  * Represents the 2-d game grid
  */
 
-public class GameGrid extends Observable{
+public class GameGrid extends Observable {
 
-	Node.OccupiedBy[][] grid;
-	private int[][] neighborGrid;
+	Node[][] grid;
 	private int highestPoints = 0;
 	private int size;
 	public static final int INROW = 5; // Squares to win.
 
+	private HashMap<Node.OccupiedBy, Integer> points; // Keep the score of all players.
+
+	public static void main(String[] args) {
+		GameGrid grid = new GameGrid(10);
+		grid.move(0, 0, Node.OccupiedBy.ME);
+		grid.move(1, 1, Node.OccupiedBy.ME);
+
+		grid.move(2, 2, Node.OccupiedBy.ME);
+
+		grid.move(3, 3, Node.OccupiedBy.ME);
+
+		grid.move(4, 4, Node.OccupiedBy.ME);
+
+		System.out.println(grid.isWinner(Node.OccupiedBy.ME));
+
+	}
+
 	/**
 	 * Constructor
 	 * 
-	 * @param size The width/height of the game grid
+	 * @param size
+	 *            The width/height of the game grid
 	 */
-	public GameGrid(int size){
-		this.grid = new Node.OccupiedBy[size][size];
-		this.neighborGrid = new int[size][size];
+	public GameGrid(int size) {
 		this.size = size;
+		this.grid = new Node[size][size];
+		for (int x = 0; x < this.grid.length; x++) {
+			for (int y = 0; y < this.grid[x].length; y++) {
+				this.grid[x][y] = new Node(Node.OccupiedBy.EMPTY, x, y);
+			}
+		}
+
+		// Init score counter.
+		this.points = new HashMap<>();
+		for (Node.OccupiedBy o : Node.OccupiedBy.values()) {
+			this.points.put(o, 0);
+		}
+
 	}
-	
+
 	/**
 	 * Reads a location of the grid
 	 * 
-	 * @param x The x coordinate
-	 * @param y The y coordinate
+	 * @param x
+	 *            The x coordinate
+	 * @param y
+	 *            The y coordinate
 	 * @return the value of the specified location
 	 */
-	public Node.OccupiedBy getLocation(int x, int y){
-		return this.grid[x][y];
+	public Node.OccupiedBy getLocation(int x, int y) {
+		return this.grid[x][y].getValue();
 	}
-	
+
 	/**
 	 * Returns the size of the grid
 	 * 
 	 * @return the grid size
 	 */
-	public int getSize(){
+	public int getSize() {
 		return this.size;
 	}
-	
+
 	/**
 	 * Enters a move in the game grid
 	 * 
-	 * @param x the x position
-	 * @param y the y position
+	 * @param x
+	 *            the x position
+	 * @param y
+	 *            the y position
 	 * @param player
 	 * @return true if the insertion worked, false otherwise
 	 */
-	public boolean move(int x, int y, Node.OccupiedBy player){
+	public boolean move(int x, int y, Node.OccupiedBy player) {
 		boolean result = true;
 		if (this.getLocation(x, y) == Node.OccupiedBy.EMPTY) {
-			this.grid[x][y] = player;
+			this.grid[x][y].setValue(player);
+
+			int newScore = this.grid[x][y].getMaximumChain(this.grid);
+
+			if (newScore > this.points.get(player)) {
+				this.points.put(player, newScore);
+			}
+
 			this.updateNeighbors(x, y);
 			this.setChanged();
 			this.notifyObservers();
 		} else {
 			result = false;
-		}		
-		
+		}
+
 		return result;
 	}
-	
+
 	/**
 	 * Clears the grid of pieces
 	 */
-	public void clearGrid(){
+	public void clearGrid() {
 		for (int x = 0; x < this.grid.length; x++) {
 			for (int y = 0; y < this.grid[x].length; y++) {
-				this.grid[x][y] = Node.OccupiedBy.EMPTY;
+				this.grid[x][y].setValue(Node.OccupiedBy.EMPTY);
 			}
 		}
 		this.setChanged();
 		this.notifyObservers();
 	}
-	
+
 	private void updateNeighbors(int x, int y) {
 		if ((x == 0) || (x == this.size)) {
 			this.checkX(-1);
 		}
 	}
-	
+
 	private boolean checkX(int direction) {
 		return false;
 	}
-	
+
 	/**
 	 * Check if a player has 5 in row
 	 * 
-	 * @param player the player to check for
+	 * @param player
+	 *            the player to check for
 	 * @return true if player has 5 in row, false otherwise
 	 */
-	public boolean isWinner(Node.OccupiedBy player){
+	public boolean isWinner(Node.OccupiedBy player) {
+		if (this.points.get(player) >= INROW) {
+			return true;
+		}
 		return false;
 	}
 }
